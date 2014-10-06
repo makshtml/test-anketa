909eae097638cde20e845861d0444d1726d05db6
makshtml <maksim12345@gmail.com>
2014-10-06 16:43:04 +0300

    first-commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+  $( "#slider-range-max" ).slider({
+    range: "max",
+    min: 1,
+    max: 4,
+    value: 2
+  });
+// $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+});
